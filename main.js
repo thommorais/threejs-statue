ae71786e9b71628f644bfcf2d3ea44f79423fb08
thommorais <thommorais@gmail.com>
2022-12-28 19:30:52 -0300

    change to human mage

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import './style.css'
 import main from './scene/index'
 ;(async () => {
 	const api = await main({
-		character: 'angel',
+		character: 'human-mage',
 		sectionSelectors: '.chapter',
 		scrollSelector: '.container-3d',
 		onModelLoading: (percentage) => percentage,
