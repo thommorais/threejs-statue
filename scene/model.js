0efee70415a7b8214124cccd8dd18062fe3ba7c8
Thom Morais <thommorais@gmail.com>
2023-03-19 20:56:52 -0300

    update scripts

--- scene/model.js
+++ scene/model.js
@@ -8,52 +8,67 @@ const deltaEl = document.querySelector('.delta')
 
 function getModel(modelPath, store) {
     const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
-    dracoLoader.preload();
+
+    try {
+        dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+        dracoLoader.preload();
+    }catch (e) {
+        deltaEl.innerHTML = `error: ${e}`
+    }
 
     const loader = new GLTFLoader();
     loader.setDRACOLoader(dracoLoader);
+    try {
 
-    return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
 
-        if (!modelPath) {
-            reject(new Error('modelPath is required'));
-        }
+            if (!modelPath) {
+                reject(new Error('modelPath is required'));
+            }
 
-        loader.load(
-            modelPath,
-            (gltf) => {
-                const boxMaterial = new MeshStandardMaterial({
-                    roughness: 0.455,
-                    metalness: 0.475,
-                });
-                const box = gltf.scene;
+            loader.load(
+                modelPath,
+                (gltf) => {
+                    const boxMaterial = new MeshStandardMaterial({
+                        roughness: 0.455,
+                        metalness: 0.475,
+                    });
 
-                box.traverse((child) => {
-                    if (child.isMesh) {
+                    const box = gltf.scene;
+
+                    box.traverse((child) => {
+                        if (child.isMesh) {
+                            // eslint-disable-next-line no-param-reassign
+                            child.material = boxMaterial;
+                        }
                         // eslint-disable-next-line no-param-reassign
-                        child.material = boxMaterial;
-                    }
-                    // eslint-disable-next-line no-param-reassign
-                    child.castShadow = true;
-                    // eslint-disable-next-line no-param-reassign
-                    child.receiveShadow = true;
-                });
+                        child.castShadow = true;
+                        // eslint-disable-next-line no-param-reassign
+                        child.receiveShadow = true;
+                    });
 
-                resolve(box);
-            },
-            // called while loading is progressing
-            (xhr) => {
-                const loadingProgress = Math.round((xhr.loaded / xhr.total) * 100);
-                deltaEl.innerHTML = `loaded: ${xhr.loaded} <br> total: ${xhr.total}`
-                // store.setState({ loadingProgress });
-            },
-            (error) => {
-                deltaEl.innerHTML = `error: ${error}`
-                reject(error)
-            },
-        );
-    });
+                    resolve(box);
+                },
+                // called while loading is progressing
+                (xhr) => {
+                    const loadingProgress = Math.round((xhr.loaded / xhr.total) * 100);
+                    store.setState({ loadingProgress });
+
+                    deltaEl.innerHTML = `
+                     loaded: ${xhr.loaded} <br>
+                     total: ${xhr.total} <br>
+                     damp: ${JSON.stringify(xhr)}
+                `
+                },
+                (error) => {
+                    deltaEl.innerHTML = `error: ${error}`
+                    reject(error)
+                },
+            );
+        });
+    } catch (error) {
+        deltaEl.innerHTML = `error: ${error}`
+    }
 }
 
 export default getModel;
