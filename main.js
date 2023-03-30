58f567d3f3700a57d43e70cae0df137e5a68a198
Thom Morais <thommorais@gmail.com>
2023-03-29 21:19:15 -0300

    add try/catch block to test if there is any error on the app on mobile

--- main.js
+++ main.js
@@ -28,34 +28,39 @@ if (['barbarian', 'demon', 'mage'].includes(characterClass)) {
 
 
 
-const myScene = new Scene();
+try {
+	const myScene = new Scene();
 
-myScene.init({
-	characterClass,
-	characterPath: `${characterClass}/scene.glb`,
-	cameraPositionsPath: `${characterClass}/camera.json`,
-	sectionSelectors: '.chapter',
-	scrollSelector: '.container-3d'
-});
+	myScene.init({
+		characterClass,
+		characterPath: `${characterClass}/scene.glb`,
+		cameraPositionsPath: `${characterClass}/camera.json`,
+		sectionSelectors: '.chapter',
+		scrollSelector: '.container-3d'
+	});
 
-myScene.store.setState({
-	bgTexturePath: 'smoke-o.webp',
-});
+	myScene.store.setState({
+		bgTexturePath: 'smoke-o.webp',
+	});
 
-myScene.subscribe(({ modelLoadingProgress, scrollable, modelAdded }) => {
-	if (modelLoadingProgress === 100 && scrollable && modelAdded) {
+	myScene.subscribe(({ modelLoadingProgress, scrollable, modelAdded }) => {
+		if (modelLoadingProgress === 100 && scrollable && modelAdded) {
 
-		myScene.setCameraPose({ from: 0, to: 1 }).then(() => {
+			myScene.setCameraPose({ from: 0, to: 1 }).then(() => {
 
-		})
+			})
 
-		// myScene.setScenePose({ from: 4, to: 0, duration: 500 }).then(({ cameraCurrentPose, sectionCurrent }) => {
-		// 	console.log(cameraCurrentPose, sectionCurrent)
-		// })
+			// myScene.setScenePose({ from: 4, to: 0, duration: 500 }).then(({ cameraCurrentPose, sectionCurrent }) => {
+			// 	console.log(cameraCurrentPose, sectionCurrent)
+			// })
 
-		// myScene.setSectionScroll({ from: 4, to: 2, duration: 500 }).then(({ cameraCurrentPose, sectionCurrent }) => {
-		// 	console.log(cameraCurrentPose, sectionCurrent)
-		// })
+			// myScene.setSectionScroll({ from: 4, to: 2, duration: 500 }).then(({ cameraCurrentPose, sectionCurrent }) => {
+			// 	console.log(cameraCurrentPose, sectionCurrent)
+			// })
 
-	}
-}, ['modelLoadingProgress', 'scrollable', 'modelAdded'])
+		}
+	}, ['modelLoadingProgress', 'scrollable', 'modelAdded'])
+
+} catch (e) {
+	alert(e)
+}
